1bacb6492fdd5287c0917254377a22221843ea91
Bobby <blipco619@gmail.com>
2018-06-18 15:13:47 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const profile = require('./profile')
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use('/profile', profile)
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+  
+app.post('/thanks', (req, res) => {
+    res.render('thanks', { contact: req.body })
+});
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080');
+});
