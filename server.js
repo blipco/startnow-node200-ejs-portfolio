1a4113a9e92348a6c44dcec4d5d1f3450d31f084
Bobby <blipco619@gmail.com>
2018-06-19 09:22:06 -0700

    sendgrid connection

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const profile = require('./profile')
+require('dotenv').config()
+const sgMail = require('@sendgrid/mail');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -23,8 +25,22 @@ app.get('/contact', (req, res) => {
 });
   
 app.post('/thanks', (req, res) => {
+    // using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+console.log(process.env.SENDGRID_API_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'blipco619@gmail.com',
+  from: req.body.email,
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
     res.render('thanks', { contact: req.body })
 });
+
+
 
 app.listen(8080, () => {
     console.log('listening at http://localhost:8080');
