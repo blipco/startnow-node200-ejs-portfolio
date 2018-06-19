f48603749ee1a66c76753691d24a5754293fd0df
Bobby <blipco619@gmail.com>
2018-06-19 10:28:19 -0700

    deleted profile.js

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-
 const app = express();
-
-const profile = require('./profile')
-require('dotenv').config()
+require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
-app.use('/profile', profile)
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -20,27 +16,26 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
 app.get('/contact', (req, res) => {
     res.render('contact');
 });
   
 app.post('/thanks', (req, res) => {
-    // using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-console.log(process.env.SENDGRID_API_KEY)
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'blipco619@gmail.com',
-  from: req.body.email,
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-sgMail.send(msg);
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+    to: 'blipco619@gmail.com',
+    from: req.body.email,
+     subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    sgMail.send(msg);
     res.render('thanks', { contact: req.body })
 });
-
-
 
 app.listen(8080, () => {
     console.log('listening at http://localhost:8080');
